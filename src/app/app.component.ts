e3a3eb3cb6227f7171aab41d7f0894bf49e2d387
mdymen <msn@dymenstein.com>
2018-01-16 06:44:41 -0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  private listaJogos = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+   this.http.post("http://bolaocraquedebola.com.br/public/mobile/celproximojogos/?", {us_id:"3"}).
+    subscribe(res => { 
+      res.map(res => {
+        this.listaJogos.push(res);
+      })
+      console.log(this.listaJogos);
+      console.log(this.listaJogos[1]);
+    
+    });
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule, Headers } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +12,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
