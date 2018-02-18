79a5a0de10f07514d7ac515243bcd99bd3b5f63e
mdymen <msn@dymenstein.com>
2018-02-18 15:41:52 -0300

    agregar partidos a un campeonato

--- src/app/admin/adicionar-partidos/adicionar-partidos.component.ts
+++ src/app/admin/adicionar-partidos/adicionar-partidos.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-//import { CampeonatoService } from '../../entidades/campeonato.service';
 import { HttpClient } from '@angular/common/http';
 import { BackendService } from '../../backend.service';
 
@@ -13,9 +12,10 @@ export class AdicionarPartidosComponent implements OnInit {
 
   partidos = [];
   campeonatos = [];
-  equipo1 = [];
-  equipo2 = [];
+  equipos1 = [];
+  equipos2 = [];
   rodadas = [];
+  cargoCampeonato = false;
 
   constructor(private http: HttpClient, private backend: BackendService) { }
 
@@ -29,20 +29,54 @@ export class AdicionarPartidosComponent implements OnInit {
         for (let campeonato of campeonatos) {
           this.campeonatos.push(campeonato);
         }
-
+        
       });
+
+      let partido = new Partido(null,null,null,null,null);
+      this.partidos.push(partido);
+
   }
 
   seleccionarCampeonato(campeonato) {
-    console.log(campeonato);
     this.http.post(this.backend.getBackEndAdmin() + "index/adicionarpartido?", {champ:campeonato})
       .subscribe(result => {
-        console.log(result);
+
+        this.cargoCampeonato = true;
+
+        let auxiliarRodadas = result['rondas'];
+        for (let rodada of auxiliarRodadas) {
+          this.rodadas.push(rodada);
+        }
+
+        result['teams'].map(team => {
+          this.equipos1.push(team);
+          this.equipos2.push(team);
+        })
+
       })
   }
 
 
+  onSubmitPartidos(partidos) {
+    this.http.post(this.backend.getBackEndAdmin() + "index/salvarpartidos", {partidos:this.partidos})
+    .subscribe(result => {
+      console.log(result);
+    })    
+  }
+
+  adicionarjogo() {
+    console.log("adicionar partido");
+    let nuevoPartido = new Partido(null, null, null, null, null);
+    this.partidos.push(nuevoPartido);
+  }
+
+}
 
 
+export class Partido {
+
+  constructor(private ronda:number, private date:string, private hora:string, private team1:number, private team2:number) {
+
+  }
 
 }
