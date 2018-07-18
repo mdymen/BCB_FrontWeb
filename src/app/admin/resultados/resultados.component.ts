dc230ae0a5587601b9ece12bf6bb721885aa01c9
mdymen <msn@dymenstein.com>
2018-07-17 21:06:11 -0300

    campeonato brasileirao pela globo

--- src/app/admin/resultados/resultados.component.ts
+++ src/app/admin/resultados/resultados.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CampeonatoService } from '../../services/campeonato.service';
 import { PartidoService } from '../../services/partido.service';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { Global } from '../../config/global.service';
 
 @Component({
   selector: 'app-resultados',
@@ -12,25 +14,64 @@ export class ResultadosComponent implements OnInit {
   url;
   partidos;
 
-  constructor(private _campeonatoService: CampeonatoService,
-    private _partidoService:PartidoService) {
+  campeonatos;
+  rodadas;
 
+  campeonato;
+  rodada;
+  cambio;
+
+  url_img;
+
+  constructor(private _campeonatoService: CampeonatoService,
+    private _partidoService:PartidoService,
+    private spinner:Ng4LoadingSpinnerService) {
+
+      this.url_img = Global.URL_BOLAO + Global.ASSETS_EQUIPOS;
   }
 
   ngOnInit() {
-    this.search();
-  }
-
-  change(url) {
-    this.url = url;
+    this.loadCampeonatos();
   }
 
   search() {
-    this._campeonatoService.loadGlobo("http://globoesporte.globo.com/servico/backstage/esportes_campeonato/esporte/futebol/modalidade/futebol_de_campo/categoria/profissional/campeonato/campeonato-brasileiro/edicao/campeonato-brasileiro-2018/fases/fase-unica-seriea-2018/rodada/12/jogos.html", 24, 220)
+    this.spinner.show();
+    this._campeonatoService.loadGlobo(this.cambio, this.campeonato, this.rodada)
       .subscribe((res: any) => {
         this.partidos = res.body;
+        this.spinner.hide();
         console.log(res);
       })
+  }
+
+  /**
+   * Al seleccionar un campeonato retorna todas las rodadas del campeonato
+   * @param idCampeonato 
+   */
+  loadRodadas(idCampeonato) {
+    this.spinner.show();
+    this._campeonatoService.loadRodadasByCampeonato(idCampeonato)
+      .subscribe((res:any) => {
+        this.campeonato = idCampeonato;
+        this.rodadas = res.body;
+        this.spinner.hide();        
+      })
+  }
+
+  /**
+   * Carga todos los campeonatos activos disponibles
+   */
+  loadCampeonatos() {
+    this.spinner.show();
+    this._campeonatoService.getCampeonatos()
+      .subscribe((res:any) => {        
+        this.campeonatos = res;
+        this.spinner.hide();
+      })
+  }
+
+  setRodada(idRodada) {
+    this.rodada = idRodada;
   }
 
 }
--- src/app/config/global.service.ts
+++ src/app/config/global.service.ts
@@ -4,5 +4,7 @@ import { Injectable } from "@angular/core";
 export class Global {
 
     public static BACKEND = "http://www.dymenstein.com/public";
+    public static ASSETS_EQUIPOS = "/assets/equipos/grande";
+    public static URL_BOLAO = "http://www.bolaocraquedebola.com.br";
 
 }
